fab150722323abdc78087b4e5b1e6ed00d7f62c1
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-26 15:36:14 +0800

    implement sphere

---
+++ lib/sphere.ml
@@ -0,0 +1,27 @@
+open Vec
+open Ray
+open Hitrec
+open Option
+
+type sphere = { center : vec3; radius : float; }
+
+let hit (r : ray) (s : sphere) : hit_record option =
+  let d = r.origin -| s.center in
+  let a = vec_dot r.direction r.direction in
+  let b = 2.0 *. vec_dot r.direction d in
+  let c = (vec_dot d d) -. (s.radius *. s.radius) in
+  let delta = b *. b -. 4.0 *. a *. c in
+  if delta +. Float.epsilon < 0.0 then none
+  else
+    let k = Float.min
+    ((Float.neg b -. Float.sqrt delta) /. (2.0 *. a))
+    ((Float.neg b +. Float.sqrt delta) /. (2.0 *. a)) in
+    if k +. Float.epsilon < 0.0 then none
+    else
+      let hit_point = r.origin +| (k *| r.direction) in
+      let normal = (hit_point -| s.center) |/ s.radius in
+      some {
+        dist = k;
+        hit_point = hit_point;
+        normal = normal;
+      }
---
+++ lib/sphere.mli
@@ -0,0 +1,2 @@
+type sphere = { center : Vec.vec3; radius : float; }
+val hit : Ray.ray -> sphere -> Hitrec.hit_record option
