3c7291d922abe20fe464f705aab3e236f25863b2
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-28 17:36:44 +0800

    use direct and indirect sampling

--- bin/main.ml
+++ bin/main.ml
@@ -7,29 +7,52 @@ open Simplert.Sampling
 
 let prob = 0.8
 
-let rec trace (r : ray) : vec3 = 
-  match hit_objs r world with
-  | None -> (0.0, 0.0, 0.0) (* TODO: add sky texture *)
-  | Some (hitrec, mat) -> 
-    let wo = vec_neg r.direction in
-    match mat with
-    | Light light -> light wo (* TODO: transform tbn to xyz here *)
-    | BRDF brdf ->
-      if Random.float 1.0 < prob then
-        let (t, b) = tbn hitrec.normal in
-        let ((wi_x, wi_y, wi_z), pdf) = sample_direction_semi_sphere (Random.float 1.0) (Random.float 1.0) in
-        let wi = (wi_x *| t) +| (wi_y *| b) +| (wi_z *| hitrec.normal) in
-        let cosine = wi_z in
-        (vec_mul (trace { origin = hitrec.hit_point; direction = wi; }) (brdf wi wo)) |* cosine |/ pdf |/ prob
-      else
-        (0.0, 0.0, 0.0)
+let rec shade (p : vec3) (norm : vec3) (brdf : vec3 -> vec3 -> vec3) (wo : vec3) : vec3 = 
+  let l_dir : vec3 = 
+    match sample_shape lights.shape with
+    | (point, pdf) -> 
+      let wi = vec_normalized (point -| p) in
+      let r = { origin = p; direction = wi; } in
+      match hit_objs r world with
+      | None -> (0.0, 0.0, 0.0)
+      | Some (hitrec, mat) ->
+        if vec_len (hitrec.hit_point -| point) < Float.epsilon then
+          match mat with
+          | Light light -> 
+            let cosine1 = vec_cosine norm wi in
+            if cosine1 < 0.0 then (0.0, 0.0, 0.0)
+            else
+              let cosine2 = vec_cosine hitrec.normal (vec_neg wi) in
+              let sqr_dist = vec_dot (point -| p) (point -| p) in
+              (vec_mul (light (vec_neg wi)) (brdf wi wo)) |* cosine1 |* cosine2 |/ sqr_dist |/ pdf
+          | BRDF _ -> (0.0, 0.0, 0.0)
+        else (0.0, 0.0, 0.0)
+  in
+  let l_indir =
+    if Random.float 1.0 < prob then
+      let (t, b) = tbn norm in
+      let ((wi_x, wi_y, wi_z), pdf) = sample_direction_semi_sphere (Random.float 1.0) (Random.float 1.0) in
+      let wi = (wi_x *| t) +| (wi_y *| b) +| (wi_z *| norm) in
+      let cosine = wi_z in
+      let r = { origin = p; direction = wi; } in
+      match hit_objs r world with
+      | None -> (0.0, 0.0, 0.0)
+      | Some (hitrec, mat) ->
+        match mat with
+        | Light _ -> (0.0, 0.0, 0.0)
+        | BRDF brdf_next ->
+          (vec_mul (shade hitrec.hit_point hitrec.normal brdf_next (vec_neg wi)) (brdf wi wo)) |* cosine |/ pdf |/ prob
+    else
+      (0.0, 0.0, 0.0)
+  in
+  l_dir +| l_indir
 
 let () =
   Random.self_init();
   let width = 400 in
   let height = 400 in
   let ratio = 0.005 in
-  let multi_sampling_per_pix = 50 in
+  let multi_sampling_per_pix = 100 in
   let eye = (0.0, -1.0, 1.0) in
   let lookat = (0.0, 1.0, 0.0) in
   let up = vec_normalized (0.0, 0.0, 1.0) in
@@ -43,7 +66,15 @@ let () =
       List.init multi_sampling_per_pix (fun _ -> 
         let gamma = 0.5 in
         let shade_point = corner +| ((float_of_int y +. Random.float 1.0) *. ratio *| axis_y) +| ((float_of_int x +. Random.float 1.0) *. ratio *| axis_x) in
-        vec_gamma_correction gamma (trace { origin = eye; direction = vec_normalized (shade_point -| eye) }) |* 255.0
+        vec_gamma_correction gamma (
+          let wo = vec_normalized (shade_point -| eye) in
+          match hit_objs { origin = eye; direction = wo; } world with
+          | None -> (0.0, 0.0, 0.0)
+          | Some (hitrec, mat) ->
+            match mat with
+            | Light light -> light (vec_neg wo)
+            | BRDF brdf -> shade hitrec.hit_point hitrec.normal brdf (vec_neg wo)
+        ) |* 255.0
       )
     ) |/ (float_of_int multi_sampling_per_pix))
   in
